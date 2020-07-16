3b90998e3d88deeda64d9039cea06bf988a49825
Illia Batozskyi <ivz@amazon.com>
2020-07-16 11:42:20 -0400

    Make CachedProperty decorator thread safe

--- botocore/utils.py
+++ botocore/utils.py
@@ -26,6 +26,7 @@ import cgi
 
 import dateutil.parser
 from dateutil.tz import tzutc
+from threading import RLock
 
 import botocore
 import botocore.awsrequest
@@ -709,16 +710,53 @@ class CachedProperty(object):
 
     """
 
-    def __init__(self, fget):
-        self._fget = fget
+    _NOT_FOUND = object()
 
-    def __get__(self, obj, cls):
-        if obj is None:
+    def __init__(self, func):
+        self.func = func
+        self.attrname = None
+        self.__doc__ = func.__doc__
+        self.lock = RLock()
+
+    def __set_name__(self, owner, name):
+        if self.attrname is None:
+            self.attrname = name
+        elif name != self.attrname:
+            raise TypeError(
+                "Cannot assign the same cached_property to two different names "
+                f"({self.attrname!r} and {name!r})."
+            )
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
             return self
-        else:
-            computed_value = self._fget(obj)
-            obj.__dict__[self._fget.__name__] = computed_value
-            return computed_value
+        if self.attrname is None:
+            raise TypeError(
+                "Cannot use cached_property instance without calling __set_name__ on it.")
+        try:
+            cache = instance.__dict__
+        except AttributeError:  # not all objects have __dict__ (e.g. class defines slots)
+            msg = (
+                f"No '__dict__' attribute on {type(instance).__name__!r} "
+                f"instance to cache {self.attrname!r} property."
+            )
+            raise TypeError(msg) from None
+        val = cache.get(self.attrname, self._NOT_FOUND)
+        if val is self._NOT_FOUND:
+            with self.lock:
+                # check if another thread filled cache while we awaited lock
+                val = cache.get(self.attrname, self._NOT_FOUND)
+                if val is self._NOT_FOUND:
+                    val = self.func(instance)
+                    try:
+                        cache[self.attrname] = val
+                    except TypeError:
+                        msg = (
+                            f"The '__dict__' attribute on {type(instance).__name__!r} instance "
+                            f"does not support item assignment for caching {self.attrname!r} property."
+                        )
+                        raise TypeError(msg) from None
+        return val
 
 
 class ArgumentGenerator(object):
